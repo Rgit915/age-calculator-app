bc3a44096eae75529b231bdde86ad2687768f9d1
Rgit915 <rora.goitom@gmail.com>
2024-08-27 13:30:39 -0400

    Create DisplayAge component for displaying age values

---
+++ src/components/DisplayAge.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const DisplayAge = () => {
+  return (
+    <section className='age'>
+      <div className="age-display">
+        <div className="age-years">
+          <p><span> - - </span> years</p>
+        </div>
+        <div className="age-months">
+          <p><span> - - </span>months</p>
+        </div>
+        <div className="age-days">
+          <p><span> - - </span> days</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default DisplayAge
