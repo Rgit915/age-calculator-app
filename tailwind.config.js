1040175aee7539052871dc69ce69050822622d1e
Rgit915 <rora.goitom@gmail.com>
2024-08-27 12:53:12 -0400

    Add custom border-radius, colors, and font in Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'custom': '24px 24px 100px 24px',
+      },
+      colors: {
+        "purple": "hsl(259, 100%, 65%)",
+        "light-red": "hsl(0, 100%, 67%)",
+        "white": "hsl(0, 0%, 100%)",
+        "off-white": "hsl(0, 0%, 94%)",
+        "light-grey": "hsl(0, 0%, 86%)",
+        "smokey-grey": "hsl(0, 1%, 44%)",
+        "off-black": "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
