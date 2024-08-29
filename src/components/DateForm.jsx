e4f70b73b27990eb18746aac32dffd0473f07552
Rgit915 <rora.goitom@gmail.com>
2024-08-29 17:15:49 -0400

    Add mobile first styles

--- src/components/DateForm.jsx
+++ src/components/DateForm.jsx
@@ -1,28 +1,31 @@
 const DateForm = () => {
   return (
-    <section className="date-of-birth">
+    <section className="date-of-birth flex flex-row uppercase text-sm">
       <div className="date-form">
         <form>
-          <div className="input-day">
-            <label htmlFor="day">Day</label>
-            <input type="number" id="day" name="day" />
+          <div className="date-input">
+            <div className="input-day ">
+              <label htmlFor="day">Day</label>
+              <input type="number" id="day" name="day" placeholder="DD" />
+            </div>
+            <div className="input-month">
+              <label htmlFor="month">Month</label>
+              <input type="number" id="month" name="month" placeholder="MM" />
+            </div>
+            <div className="input-year">
+              <label htmlFor="year">Year</label>
+              <input type="number" id="year" name="year" placeholder="YYYY" />
+            </div>
           </div>
-          <div className="input-month">
-            <label htmlFor="month">Month</label>
-            <input type="number" id="month" name="month" />
-          </div>
-          <div className="input-year">
-            <label htmlFor="year">Year</label>
-            <input type="number" id="year" name="year" />
-          </div>
-
-          <div className="arrow-button">
-            <button type="submit">
-              <img src="images/icon-arrow.svg" alt="Submit" />
-            </button>
-          </div>
-          <div className="horizontal-line">
-            <hr />
+          <div className="calculate-container">
+            <div className="arrow-button">
+              <button type="submit">
+                <img src="images/icon-arrow.svg" alt="Submit" />
+              </button>
+            </div>
+            <div className="horizontal-line w-72">
+              <hr />
+            </div>
           </div>
         </form>
       </div>
--- src/components/DisplayAge.jsx
+++ src/components/DisplayAge.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const DisplayAge = () => {
   return (
     <section className='age'>
-      <div className="age-display">
+      <div className="age-display ">
         <div className="age-years">
-          <p><span> - - </span> years</p>
+          <p><span>- -</span> years</p>
         </div>
         <div className="age-months">
-          <p><span> - - </span>months</p>
+          <p><span>- -</span> months</p>
         </div>
         <div className="age-days">
-          <p><span> - - </span> days</p>
+          <p><span>- -</span> days</p>
         </div>
       </div>
     </section>
