8e5f20fdb743922c9cd8a2fc58c7cf6970aed6ed
Rgit915 <rora.goitom@gmail.com>
2024-08-29 20:01:51 -0400

    Integrate validation logic and error handling to `DateForm` component

--- src/components/DateForm.jsx
+++ src/components/DateForm.jsx
@@ -1,21 +1,77 @@
+import { useState } from 'react';
+import { validateDate } from '../utils/dateValidation';
+
 const DateForm = () => {
+  // State variables to store the input values
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+  // State variable to store error messages
+  const [errors, setErrors] = useState({ day: '', month: '', year: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    const validationErrors = validateDate(day, month, year); // Validate the date
+    setErrors(validationErrors); // Set validation errors in the state
+
+    // If there are no validation errors, proceed with form submission logic
+    if (Object.keys(validationErrors).length === 0) {
+      // Handle successful form submission
+      console.log('Date is valid:', { day, month, year });
+    } else {
+      // Handle form submission with errors
+      console.log('Date is invalid');
+
+    }
+  }
+
   return (
     <section className="date-of-birth flex flex-row uppercase text-sm">
       <div className="date-form">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="date-input">
-            <div className="input-day ">
-              <label htmlFor="day">Day</label>
-              <input type="number" id="day" name="day" placeholder="DD" />
+            <div  className={`input-day ${errors.day ? 'text-light-red' : ''}`}>
+              <label htmlFor="day" className={`${errors.month ? 'text-light-red' : ''}`}>Day</label>
+              <input
+                type="number"
+                id="day"
+                name="day"
+                placeholder="DD"
+                onChange={(e) => setDay(e.target.value)}
+                className={`${errors.day ? 'text-light-red' : ''}`}
+              />
+            <div>
+              {errors.day && <span className="error-display">{errors.day}</span>}
             </div>
-            <div className="input-month">
-              <label htmlFor="month">Month</label>
-              <input type="number" id="month" name="month" placeholder="MM" />
+
             </div>
-            <div className="input-year">
-              <label htmlFor="year">Year</label>
-              <input type="number" id="year" name="year" placeholder="YYYY" />
+            <div className={`input-month ${errors.month ? 'text-light-red' : ''}`}>
+              <label htmlFor="month" className={`${errors.month ? 'text-light-red' : ''}`}>Month</label>
+              <input
+                type="number"
+                id="month"
+                name="month"
+                placeholder="MM"
+                onChange={(e) => setMonth(e.target.value)}
+
+                />
+                <div>
+                {errors.month && <span className="error-display">{errors.month}</span>}
+                </div>
             </div>
+            <div className={`input-year ${errors.year ? 'text-light-red' : ''}`}>
+              <label htmlFor="year" className={`${errors.year ? 'text-light-red' : ''}`}>Year</label>
+              <input
+                type="number"
+                id="year"
+                name="year"
+                placeholder="YYYY"
+                onChange={(e) => setYear(e.target.value)}
+                />
+                <div>
+                  {errors.year && <span className="error-display">{errors.year}</span>}
+                </div>
+              </div>
           </div>
           <div className="calculate-container">
             <div className="arrow-button">
