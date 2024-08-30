159b899b03e1bfe83cdc3aa0c5e45e7ca6e2f044
Rgit915 <rora.goitom@gmail.com>
2024-08-29 21:30:48 -0400

    Modify DisplayAge to render the age

--- src/components/DisplayAge.jsx
+++ src/components/DisplayAge.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const DisplayAge = () => {
+const DisplayAge = ({ age }) => {
   return (
-    <section className='age'>
-      <div className="age-display ">
-        <div className="age-years">
-          <p><span>- -</span> years</p>
-        </div>
-        <div className="age-months">
-          <p><span>- -</span> months</p>
-        </div>
-        <div className="age-days">
-          <p><span>- -</span> days</p>
-        </div>
+    <section className="age">
+    <div className="age-display">
+      <div className="age-years">
+        <p><span>{age ? age.years : '- -'}</span> years</p>
       </div>
-    </section>
-  )
-}
+      <div className="age-months">
+        <p><span>{age ? age.months : '- -'}</span> months</p>
+      </div>
+      <div className="age-days">
+        <p><span>{age ? age.days : '- -'}</span> days</p>
+      </div>
+    </div>
+  </section>
+  );
+};
 
-export default DisplayAge
+export default DisplayAge;
