3ca9714a9f9c4d3e6aa7ca7e8ec11a936a9df0c7
Rgit915 <rora.goitom@gmail.com>
2024-08-29 21:33:37 -0400

    Modify App component to manage state and handle date submissions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
+
 import DateForm from "./components/DateForm";
 import DisplayAge from "./components/DisplayAge";
+import { calculateAge } from './utils/calculateAge';
 
 const App = () => {
+  // State to store the birthDate information
+  const [age, setAge] = useState(null);
 
+  const handleDateSubmit = (date) => {
+    const calculatedAge = calculateAge(date);
+    setAge(calculatedAge);
+  };
   return (
     <main className="container">
-      <DateForm />
-      <DisplayAge />
+      <DateForm onDateSubmit={handleDateSubmit} />
+      <DisplayAge age={age} />
     </main>
   );
 };
